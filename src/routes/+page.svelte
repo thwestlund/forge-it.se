171c897896456d3fa4a06fbf52c91c78a5c67f5f
ThommyWestlund <thommy.westlund@b3.se>
2025-02-05 14:53:11 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import { fade } from 'svelte/transition'
+  import me from "$lib/assets/images/me.jpg";
+</script>
+
+<div class="mx-auto px-4 py-8 min-h-screen bg-neutral-50 dark:bg-zinc-900">
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4 max-w-6xl mx-auto">
+    <!-- Company Name - Spans full width -->
+    <div class="col-span-full bg-gray-800 dark:bg-teal-950 rounded-2xl shadow-sm p-8 hover:shadow-md transition-shadow">
+      <h1 class="text-4xl font-bold text-zinc-200 mb-1">Forge IT Solutions AB</h1>
+      <p class="text-zinc-300">Skapar lösningar, formar upplevelser</p>
+    </div>
+
+    <!-- Services - Spans 2 columns -->
+    <div class="md:col-span-2 bg-amber-200 dark:bg-sky-950 rounded-2xl shadow-sm p-8 hover:shadow-md transition-shadow">
+      <h2 class="text-2xl font-semibold text-zinc-800 dark:text-zinc-200 mb-4">Våra tjänster</h2>
+      <div class="grid grid-cols-1 sm:grid-cols-2 gap-8">
+        <div>
+          <h3 class="text-lg font-semibold text-zinc-700 dark:text-zinc-200 mb-1">Systemutveckling</h3>
+          <p class="text-zinc-600 dark:text-zinc-200">Vi utvecklar skräddarsydda lösningar med användaren i fokus.</p>
+        </div>
+        <div>
+          <h3 class="text-lg font-semibold text-zinc-700 dark:text-zinc-200 mb-1">IT-konsulttjänster</h3>
+          <p class="text-zinc-600 dark:text-zinc-200">Med vår breda erfarenhet hjälper vi er att navigera i den tekniska utvecklingen.</p>
+        </div>
+        <div>
+          <h3 class="text-lg font-semibold text-zinc-700 dark:text-zinc-200 mb-1">UX/UI-design</h3>
+          <p class="text-zinc-600 dark:text-zinc-200">Vi formger användarvänliga och estetiskt tilltalande gränssnitt som engagerar era användare.</p>
+        </div>
+        <div>
+          <h3 class="text-lg font-semibold text-zinc-700 dark:text-zinc-200 mb-1">Tillgänglighetsexpertis</h3>
+          <p class="text-zinc-600 dark:text-zinc-200">Vi säkerställer att era digitala lösningar är tillgängliga för alla användare.</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Thommy Westlund - Spans 1 column -->
+     <a href="https://thommywestlund.se" class="bg-red-300 dark:bg-lime-900 rounded-2xl p-8 shadow-[4px_4px_2px_0px_rgba(0,0,0,1)] hover:scale-95 hover:origin-bottom-right hover:shadow duration-200 decoration-zinc-800 hover:underline focus:outline-none focus:underline underline-offset-4 focus:border-8 dark:decoration-zinc-200 focus:border-red-900 focus:dark:border-lime-600">
+      <span class="font-semibold text-zinc-800 dark:text-zinc-200 text-4xl text-wrap leading-14">Thommy Westlund.se</span>
+  </a>
+    <!-- Projects - Spans 2 columns -->
+    <div class="md:col-span-2 bg-orange-100 dark:bg-orange-950 rounded-2xl shadow-sm p-8 hover:shadow-md transition-shadow">
+      <h2 class="text-2xl font-semibold text-zinc-800 dark:text-zinc-200 mb-4">Ärofyllda projekt</h2>
+      <div class="grid grid-cols-1 sm:grid-cols-2 gap-8">
+        <div>
+          <h3 class="text-lg font-semibold text-zinc-700 mb-1 dark:text-zinc-200">Kammarkollegiet</h3>
+          <p class="text-zinc-600 dark:text-zinc-200">Ett nytt system för omställningsstöd designat med helt nya användarprocesser.</p>
+        </div>
+        <div>
+          <h3 class="text-lg font-semibold text-zinc-700 dark:text-zinc-200 mb-1">Jordbruksverket</h3>
+          <p class="text-zinc-600 dark:text-zinc-200">Expertis i flera projekt så som Markuppföljningen, Geofoto och DSData</p>
+        </div>
+      </div>
+    </div>
+      <!-- Contact - Spans 1 column -->
+      <a href="mailto:thommy@forge-it.se" class="bg-blue-300 dark:bg-pink-900 rounded-2xl shadow-[4px_4px_2px_0px_rgba(0,0,0,1)] p-8 hover:origin-bottom-right hover:scale-95 hover:shadow duration-200 decoration-zinc-800 dark:decoration-zinc-200 hover:underline focus:underline focus:outline-none underline-offset-4 focus:border-8 focus:border-sky-900 focus:dark:border-pink-600">
+        <span class="font-semibold text-zinc-800 mb-4 text-4xl leading-14 dark:text-zinc-200">Skicka e-brev till mig</span>
+      </a>
+  </div>
+  <p class=" text-neutral-600 text-center text-sm dark:text-zinc-200">
+    © 2024, Forge IT Solutions AB, Org.nr. 559502-1923
+  </p>
+</div>
+
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: ["./src/**/*.{html,js,svelte,ts}", "./static/**/*.html"],
+
+  theme: {
+    extend: {
+      aspectRatio: {
+        "2/3": "2 / 3",
+      },
+      maxHeight: {
+        128: "32rem",
+        144: "36rem",
+        160: "40rem",
+        192: "48rem",
+        224: "56rem",
+        256: "64rem",
+        288: "72rem",
+        320: "80rem",
+        "screen-75": "75vh",
+        "screen-90": "90vh",
+      },
+    },
+  },
+  plugins: [],
+};
